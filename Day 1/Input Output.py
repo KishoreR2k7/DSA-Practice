adf38ef393d33ca4bf5ec2650c2009d9ab651a6b
Kishore R <kishorerajaji65@gmail.com>
2025-08-22 23:36:45 +0530

    Your commit message

---
+++ Day 1/Input Output.py
@@ -0,0 +1,7 @@
+# input get String,Integer and Float
+a=input('Enter Input String:')
+print(f'Output:{a}')
+b=int(input('Enter Input Integer:'))
+print(f'Output:{b}')
+c=float(input('Enter Input Float:'))
+print(f'Output:{c}')
---
+++ Day 1/Type Conversion.py
@@ -0,0 +1,13 @@
+# Data conversion in normal method
+a=input('EnterInput:')
+print(f'Datatype of a :{type(a)}\n')
+
+# string to int
+b=int(a)
+print(b)
+print(f'Datatype of b :{type(b)}\n')
+
+# string to float
+c=float(a)
+print(f'Float:{c}')
+print(f'Datatype of c :{type(c)}')
